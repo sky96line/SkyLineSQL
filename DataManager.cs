25455bafee648c30317361c6d5815a87bc3ce9de
Akash Buch <akash.buch@devitpl.com>
2025-05-31 12:20:49 +0530

    error fix

--- DataManager.cs
+++ DataManager.cs
@@ -184,7 +184,7 @@ namespace CopyWindowSQL
         #region Notify
 
         /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged"/>
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
         {
@@ -193,7 +193,7 @@ namespace CopyWindowSQL
             PropertyChanged?.Invoke(this, e);
         }
 
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
         }
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -197,7 +197,7 @@ namespace CopyWindowSQL
         #region Notify
 
         /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged"/>
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
         {
@@ -206,7 +206,7 @@ namespace CopyWindowSQL
             PropertyChanged?.Invoke(this, e);
         }
      
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
         }
